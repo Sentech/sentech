2d50317fb2779458a5cfe3b0f7e58a501c63aafd
Joeri Poesen <jpoesen@gmail.com>
2014-04-20 12:46:44 +0000

    Add HomePageView and url.

--- sentech/urls.py
+++ sentech/urls.py
@@ -4,10 +4,14 @@ from django.contrib import admin
 
 admin.autodiscover()
 
+from views import HomePageView
+
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'sentech.views.home', name='home'),
+    #url(r'^$', 'sentech.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+
+    url(r'^$', HomePageView.as_view(), name='home'),
 
     url(r'^admin/', include(admin.site.urls)),
 
